794420d3bac2d7284ede160bcff58f3d09e590b9
DGovorov <pyavchik@gmail.com>
2016-03-01 21:08:03 +0200

    01.03.2016

--- src/OOP/RectangleExample.java
+++ src/OOP/RectangleExample.java
@@ -8,20 +8,20 @@ import com.sun.org.apache.regexp.internal.RE;
 public class RectangleExample {
     public static void main(String[] args) {
         Rectangle firstRectangle = new Rectangle();
-        firstRectangle.height = 2;
-        firstRectangle.width = 3;
-        firstRectangle.color = "Blue";
+        firstRectangle.setHeight(2);
+        firstRectangle.setWidth(3);
+        firstRectangle.setColor("Blue");
 
-        int w = firstRectangle.width;
+        int w = firstRectangle.getWidth();
         int a = firstRectangle.calculateArea();
 
-        System.out.println(firstRectangle.height);
-        System.out.println(firstRectangle.color);
+        System.out.println(firstRectangle.getHeight());
+        System.out.println(firstRectangle.getColor());
 
         Rectangle secondRectangle = new Rectangle();
-        secondRectangle.height = 5;
-        secondRectangle.width = 10;
-        System.out.println("rectangle's color is " + secondRectangle.color);
+        secondRectangle.setHeight(5);
+        secondRectangle.setWidth(10);
+        System.out.println("rectangle's color is " + secondRectangle.getColor());
 
         secondRectangle.calculateArea();
 
@@ -39,19 +39,43 @@ public class RectangleExample {
         Rectangle thirdRectangle = null;
 
        // thirdRectangle.calculateArea();
-        firstRectangle.width = 4;
+        firstRectangle.setWidth(4);
 
+/*        Rectangle fourthRectangle = null;
+        System.out.println(thirdRectangle.color == fourthRectangle.color);*/
 
+       // anotherMethod();
 
+        //System.out.println("======>" + firstRectangle);
+
+        System.out.println("first rect is " + firstRectangle.toString());
+
+        Rectangle anotherRectangle = new Rectangle(firstRectangle.getWidth(), firstRectangle.getHeight());
+        System.out.println("another rectangle is " + anotherRectangle.toString());
+
+        System.out.println(firstRectangle == anotherRectangle);
+
+        System.out.println(firstRectangle.equals(anotherRectangle));
 
     }
+
+/*    public static void anotherMethod (){
+        int i = 10;
+        System.out.println("sdasf");
+        anotherAnotherMethod();
+    }
+
+    public static void anotherAnotherMethod(){
+        Rectangle rectangle = null;
+        rectangle.calculateArea();
+    }*/
 
     public static int calculateArea(int height, int width) {
         return height * width;
     }
 
     public static int calculateArea(Rectangle rectangle) {
-        return rectangle.height * rectangle.width;
+        return rectangle.getHeight() * rectangle.getWidth();
     }
 
 }
