6c0b94c4d558b231cd2db7d0a2d13578dfcd2c20
DGovorov <pyavchik@gmail.com>
2016-04-26 21:06:07 +0300

    Sockets, Multithreading

--- src/sockets/Client.java
+++ src/sockets/Client.java
@@ -1,7 +1,5 @@
 package sockets;
 
-import sun.misc.OSEnvironment;
-
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
@@ -33,14 +31,14 @@ public class Client {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String message = scanner.next();
-            if (message.equals("exit")){
+            if (message.equals("exit")) {
                 break;
             }
             send(message);
         }
     }
 
-    private void send (String message){
+    private void send(String message) {
         try (
                 Socket socket = new Socket("192.168.1.111", 3502);
                 PrintWriter writer = new PrintWriter(socket.getOutputStream())
